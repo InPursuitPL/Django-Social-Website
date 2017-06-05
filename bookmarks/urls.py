31fbfe292c8197dd452fbb7e27d3934f5d5d8560
InPursuitPL <inpursuitpl@gmail.com>
2017-06-05 11:55:40 +0200

    Works on bookmarklet and static files - in progress.

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -5,9 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'account/', include('account.urls')),
+    url(r'^account/', include('account.urls')),
+    url(r'^images/', include('images.urls', namespace='images')), 
     url('social-auth/',
-        include('social.apps.django_app.urls', namespace='social'))
+        include('social.apps.django_app.urls', namespace='social')),
+
 ]
 
 if settings.DEBUG:
